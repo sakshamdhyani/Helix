abfe3f4ad4670dafbc73f3e2985078365097f2a0
Saksham <saksham@traffury.in>
2026-07-12 19:11:00 +0530

    initial commit

---
+++ controller/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
